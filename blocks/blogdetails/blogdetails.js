cb24c14e7baae43f3eb9f1711dbc45557cc1ad2e
jain12swati <swatijain@adobe.com>
2024-01-31 17:29:18 +0530

    home page fixes

--- blocks/blogdetails/blogdetails.js
+++ blocks/blogdetails/blogdetails.js
@@ -39,7 +39,7 @@ export default async function decorate(block) {
                                     <div class="banner-image"> 
                                         <img src="https://images.unsplash.com/photo-1513542789411-b6a5d4f31634?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="banner-image">
                                     </div>
-                                    <div class="">
+                                    <div class="blog-text">
                                     ${element.blogdetails}
                                     </div>
                     `;
--- blocks/pagelayout/pagelayout.js
+++ blocks/pagelayout/pagelayout.js
@@ -28,8 +28,10 @@ export default async function decorate(block) {
                        <div><a href="/blog-details?title=${el.title}"  class="blog-title">${el.title}</a></div>
                       <div class="blog-credit">by <span>${el.firstname} ${el.lastname}</span> ${el.date} </div>
                       <div class="blog-body">
-                      <img src="https://images.unsplash.com/photo-1513542789411-b6a5d4f31634?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
-                      <p class="blog-details">${el.blogdetails}</p>
+                       <div class="blog-image">
+                       <img src="https://images.unsplash.com/photo-1513542789411-b6a5d4f31634?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                        </div>
+                       <div class="blog-details">${(el.blogdetails).slice(0, 200)+"..."}</div>
                       
                       </div>
                       <a href="/blog-details?title=${el.title}" class="read-more">Read More...</a>
