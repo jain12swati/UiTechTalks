f5df806730bfe04035622809a909a4004f6e37cc
jain12swati <swatijain@adobe.com>
2024-02-01 16:30:03 +0530

    comments form added

---
+++ blocks/postcomment/postcomment.js
@@ -0,0 +1,9 @@
+export default async function decorate(block){
+    const   commentwrapper = block.querySelector("a");
+    console.log("hi", commentwrapper);
+
+    const commentFormUrl = commentwrapper.href
+    
+
+
+}
